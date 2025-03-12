36ef01a807fc4a0268a57e21dee1ac3b109960f3
Lennar <lennar.aburto.erh@gmail.com>
2025-03-12 01:42:22 -0300

    primera actualización

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = []
+
+
+function agregarAmigo() {
+    let amigo = document.getElementById("amigo").value;
+
+    if (amigo) {
+        amigos.push(amigo);
+        mostrarAmigos();
+        document.getElementById("amigo").value = "";
+    } else {
+        alert("Por favor, ingresa un nombre.");
+    }
+}
+
+function mostrarAmigos() {
+    let elementoHTML = document.getElementById("listaAmigos");
+    elementoHTML.innerHTML = amigos.join(", ");}
